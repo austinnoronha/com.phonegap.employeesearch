5d1ce67cb4b6930ae95709bc2baa4b45b00bac10
Austin Noronha <austin_noronha@hotmail.com>
2015-07-10 11:07:09 +0530

    Using HomeView js as class view

--- js/main.js
+++ js/main.js
@@ -1,27 +1,13 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(app.employeeLiTpl(employees));
-        });
-    },
-
     initialize: function() {
-
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
+        var self = this;
         this.store = new MemoryStore(function() {
-            app.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+
 
     showAlert: function (message, title) {
         if (navigator.notification) {
