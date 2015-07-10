893ce10e80cb32a19afb624cc41ccc7d03deaf90
Austin Noronha <austin_noronha@hotmail.com>
2015-07-10 10:55:40 +0530

    Add new template engine HandleBars.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(app.employeeLiTpl(employees));
         });
     },
 
     initialize: function() {
+
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+
         this.store = new MemoryStore(function() {
             app.renderHomeView();
         });
@@ -21,14 +19,7 @@ var app = {
     },
 
     renderHomeView: function() {
-        var html =
-            "<div class='header'><h1>Employee Search:</h1></div>" +
-                "<div class='search-view'>" +
-                "<label>Search: <input class='search-key'/></label>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>" +
-                "<footer class='footer'>&copy; 2015 Regards Austin</footer>";
-        $('body').html(html);
+        $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
